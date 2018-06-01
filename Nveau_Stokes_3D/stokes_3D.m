15a6dde5a811e8d210e3e7db813fcd9f0e7ba0ba
Pierre Gauthier <gauthie55u@etu.univ-lorraine.fr>
2018-06-01 19:54:20 +0200

    modification de stokes 3D

--- Nveau_Stokes_3D/stokes_3D.m
+++ Nveau_Stokes_3D/stokes_3D.m
@@ -59,21 +59,25 @@ A=[nu*(2*DxDx+DyDy+DzDz),              nu*DxDy',              nu*DxDz',         
 fprintf('%g s\n',toc(tstart1)); 
 
 % Construction du second membre (RHS)
-x=v(:,1); y=v(:,2); z=v(:,3);  % coordonnées
+[fx, fy, fz, fxgd, fygd,fzgd,fxy] = f2Dto3D_2(v,t,nv1,nv2,nbquad,X,Y,Z,Nx,Ny,Nz); 
+%   fx, fy, fz sont les forces dans des vecteurs colonnes 1*size(v,1)
+%   fxgd, fygd, fzgd sont les forces dans des matrices Nx*Ny*Ny pour
+%   affichage
+
 
 % terme source f sans pression
-[fx,fy,fz]=source_f(nu,x,y,z);
-F=[Masse*fx; Masse*fy; Masse*fz; sparse(nv1,1)];
+
+F=[fx; fy; fz; sparse(nv1,1)];
 
 % Traitement de la pression à moyenne nulle par multiplicateur de Lagrange.
-[A,F,nv1]=tmtpression(Nx,Ny,Nz,nv1,nv2,t,A,F);
+[A,F,nv1] = tmtpression(Nx,Ny,Nz,nv1,nv2,t,A,F);
 
 % Traitement des CL
 ibT=[ind_bd;ind_bd+nv2;ind_bd+2*nv2];
 icT=(1:3*nv2+nv1);
 icT(ibT)=[];
 F(ibT)=[];
-A=A(icT,icT);
+A = A(icT,icT);
 
 fprintf('     taille %d x %d, nnz = %d\n',size(A,1),size(A,2), nnz(A)); 
 fprintf('     temps total de preparation du systeme lineaire : %g s\n',toc(tstart0));
@@ -104,7 +108,7 @@ Pr2 = build_Q2Pressure(t(1:nt1,:),Pr,nv2);
 
 % Construction du maillage Q1-isoQ2
 tQ1isoQ2 = meshQ1isoQ2(t); 
-[X2,Y2,Z2,Uxgd,Uygd,Uzgd,Pgd]=gridformat(Lx,Ly,Lz,Nx,Ny,Nz,v,Ux,Uy,Uz,Pr2);
+[X2,Y2,Z2,Uxgd,Uygd,Uzgd,Pgd] = gridformat(Lx,Ly,Lz,Nx,Ny,Nz,v,Ux,Uy,Uz,Pr2);
 
 % Sauvegarde de la vitesse et de la pression
 filename_vtk='UP3D.vtk';
@@ -128,6 +132,14 @@ if displaysol
     % xslice = [0,Lx/2,Lx]; yslice = Ly/2; zslice = [0,Lz/2,Lz];
     % figure;
     % slice(X,Y,Z,R,xslice,yslice,zslice,'nearest')
+   
+    
+%   affichage du second membre de navier Stokes
+    figure();
+    quiver3(X2,Y2,Z2,fxgd,fygd,fzgd);
+    title('champ de vitesse');
+    xlabel('X'); ylabel('Y'); zlabel('Z');
+    axis equal;
 
     % Affichage Q2 du module de la vitesse
     U_mod=sqrt(Uxgd.^2 + Uygd.^2 + Uzgd.^2);
@@ -146,30 +158,4 @@ if displaysol
     axis equal
 
 end
-%---------------------------------------------------------------
-% Comparaison de la solution approchée avec une solution exacte
-%---------------------------------------------------------------
-if compare
-    % Solution exacte 3D
-    ux_exact=(x .^ 3 .* (Lx-x) .^ 3 .* y .^ 2 .* (Ly-y) .^ 3 .* z .^ 3 .* (Lz-z) .^ 3) ./ 0.243e3 - (x .^ 3 .* (Lx-x) .^ 3 .* y .^ 3 .* (Ly-y) .^ 2 .* z .^ 3 .* (Lz-z) .^ 3) ./ 0.243e3 - (x .^ 3 .* (Lx-x) .^ 3 .* y .^ 3 .* (Ly-y) .^ 3 .* z .^ 2 .* (Lz-z) .^ 3) ./ 0.243e3 + (x .^ 3 .* (Lx-x) .^ 3 .* y .^ 3 .* (Ly-y) .^ 3 .* z .^ 3 .* (Lz-z) .^ 2) ./ 0.243e3;
-    uy_exact=(x .^ 3 .* (Lx-x) .^ 3 .* y .^ 3 .* (Ly-y) .^ 3 .* z .^ 2 .* (Lz-z) .^ 3) ./ 0.243e3 - (x .^ 3 .* (Lx-x) .^ 3 .* y .^ 3 .* (Ly-y) .^ 3 .* z .^ 3 .* (Lz-z) .^ 2) ./ 0.243e3 - (x .^ 2 .* (Lx-x) .^ 3 .* y .^ 3 .* (Ly-y) .^ 3 .* z .^ 3 .* (Lz-z) .^ 3) ./ 0.243e3 + (x .^ 3 .* (Lx-x) .^ 2 .* y .^ 3 .* (Ly-y) .^ 3 .* z .^ 3 .* (Lz-z) .^ 3) ./ 0.243e3;
-    uz_exact=(x .^ 2 .* (Lx-x) .^ 3 .* y .^ 3 .* (Ly-y) .^ 3 .* z .^ 3 .* (Lz-z) .^ 3) ./ 0.243e3 - (x .^ 3 .* (Lx-x) .^ 2 .* y .^ 3 .* (Ly-y) .^ 3 .* z .^ 3 .* (Lz-z) .^ 3) ./ 0.243e3 - (x .^ 3 .* (Lx-x) .^ 3 .* y .^ 2 .* (Ly-y) .^ 3 .* z .^ 3 .* (Lz-z) .^ 3) ./ 0.243e3 + (x .^ 3 .* (Lx-x) .^ 3 .* y .^ 3 .* (Ly-y) .^ 2 .* z .^ 3 .* (Lz-z) .^ 3) ./ 0.243e3;
-    ux_exact=1e4*ux_exact; uy_exact=1e4*uy_exact; uz_exact=1e4*uz_exact;
-    p_exact=zeros(nv2,1);
-    
-    [~,~,~,Uxgd_e,Uygd_e,Uzgd_e,Pgd_e]=gridformat(Lx,Ly,Lz,Nx,Ny,Nz,v,ux_exact,uy_exact,uz_exact,p_exact);
-    U_mod_exact=sqrt(Uxgd_e.^2 + Uygd_e.^2 + Uzgd_e.^2);
-    erreur = max(abs(U_mod_exact(:)-U_mod(:)))/max(abs(U_mod_exact(:)));
-    fprintf('*/ Comparaison avec la solution exacte :\n');
-    fprintf('   erreur relative = %6.3e\n',erreur);
-    
-    % Affichage 
-    if displaysol
-        xslice = [0,Lx/2,Lx]; yslice = Ly/2; zslice = [0,Lz/2,Lz];
-        figure()
-        slice(X2,Y2,Z2,U_mod_exact,xslice,yslice,zslice,'linear')
-        colorbar; title('Solution exacte');
-        xlabel('X'); ylabel('Y'); zlabel('Z');
-        axis equal
-    end
-end
+
